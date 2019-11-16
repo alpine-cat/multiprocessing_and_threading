02fb64d6241f4fc2f70bc62928642c4e85535f8e
Daria Trunova <trunova.bts@gmail.com>
2019-11-16 21:11:23 +0200

    job.py fixed

--- jobs.py
+++ jobs.py
@@ -5,26 +5,26 @@ import time
 
 logging.basicConfig(
     filename="app.log", 
-    level = logging.INFO, 
+    level=logging.INFO,
     filemode='w')
 
 def get_job(descript, page):
-    r = requests.get("https://jobs.github.com/positions.json", params={"description":descript, "page":page})
+    r = requests.get("https://jobs.github.com/positions.json", params={"description":descript, "page": page})
     logging.info("URL: %s", r.url)
 
 description = ["python", "java", "c#"]
 threads = []
-pages = [1,2]
+pages = [1, 2]
 for a in description:
     for page in pages:
-        threads.append(Thread(target = get_job, args = (a, page)))
+        threads.append(Thread(target=get_job, args=(a, page)))
 
 start = time.time()
 for thread in threads:
     thread.start()
-
-for thread in threads:
     thread.join()
+
+    
 
 end = time.time()
 
